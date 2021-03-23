c250b11a7a98c508e94ad41f41e83a2d787a9969
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2021-03-23 12:20:27 -0700

    weater call api

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -3,24 +3,22 @@ import { useEffect, useState } from 'react';
 
 function Weather() {
     const [person, setPerson] = useState(['']); 
-
+    const [weather, setWeather] = useState([0]);
 
     const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-
+    const cityName = "San Diego";
     useEffect(() => {
-        axios.get(`https://jsonplaceholder.typicode.com/users`)
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`)
             .then(res => {
-                const persons = res.data;
-                setPerson(persons);
+                console.log(res);
+                const cityWeather = res.data;
+                setWeather({cityWeather});
             })
     });
 
     return (
         <div>
             <h1>Weather App</h1>
-            <ul>
-                {person.map(per => <li>{per.name}</li>)}
-            </ul>
         </div>
     );
 }
