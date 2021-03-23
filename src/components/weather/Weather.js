23d396396ec3ea375a22c61451822285db369cab
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2021-03-23 11:46:26 -0700

    install axios and setup api call

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,7 +1,26 @@
-const Weather = () => {
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function Weather() {
+    const [person, setPerson] = useState(['']); 
+
+
+    const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+            .then(res => {
+                const persons = res.data;
+                setPerson(persons);
+            })
+    });
+
     return (
         <div>
             <h1>Weather App</h1>
+            <ul>
+                {person.map(per => <li>{per.name}</li>)}
+            </ul>
         </div>
     );
 }
