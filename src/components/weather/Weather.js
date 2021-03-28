4055dade8c591b64f5803ee58c76db749194f014
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2021-03-28 13:45:51 -0700

    Make api request from button

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,29 +1,21 @@
 import { useEffect, useState } from 'react';
 import Input from '../input/Input';
 
-function Weather() {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false); 
+function Weather() { 
     const [weather, setWeather] = useState([]);
     const [userInput, setUserInput] = useState("");
     const [city, setCity] = useState("");
 
     const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
-    useEffect(() => {
+    function getWeatherData(city) {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
             .then(response => response.json())
             .then((data) =>  {
-                console.log(data);
-                setIsLoaded(true);
                 setWeather(data);
-            },
-            (error) => {
-                setIsLoaded(true);
-                setError(error);
             }
         );
-    }, []);
+    }
 
     function handleChange(e) {
         console.log("It happens", e.target.value);
@@ -33,13 +25,10 @@ function Weather() {
     function submitUserData() {
         setCity(userInput);
         console.log("City Name", city);
+        getWeatherData(city);
     }
 
-    if(error) {
-        return <div>Error: {error.message}</div>;
-    } else if(!isLoaded) {
-        return <div>Loading...</div>;
-    } else {
+
 
         return (
             <div>
@@ -50,7 +39,6 @@ function Weather() {
                 <p>{userInput}</p>
             </div>
         ); 
-    }
 }
 
 export default Weather;
