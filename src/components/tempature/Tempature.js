b3656539788267650d2423c22f9a98f5afdd13ca
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2021-04-02 12:12:29 -0700

    Round Tempature

--- src/components/tempature/Tempature.js
+++ src/components/tempature/Tempature.js
@@ -2,7 +2,7 @@ const Tempature = (props) => {
     function fromKelvinToFahrenheit(props) {
         let tempature = props.temp;
         let tempatureFahrenheit = (tempature - 273.15) * 9/5 + 32
-        return tempatureFahrenheit;
+        return Number((tempatureFahrenheit).toFixed(1));
     }
 
     return <p>Tempature: {fromKelvinToFahrenheit(props)}F</p>
